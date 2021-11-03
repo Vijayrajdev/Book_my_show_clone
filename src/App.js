c25d40c8c94bd27ff1f8b70e798170ddc2a42228
Vijayraj-wd <vijayrajck@outlook.com>
2021-11-03 19:16:10 +0530

    React app with tailwind config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-10">
+      <button className="border-2 border-blue-500 bg-blue-500 text-white px-2 py-1 rounded-lg  hover:bg-transparent hover:text-blue-500 hover:border-blue-500 ">
+        Hello!!
+      </button>
     </div>
   );
 }
